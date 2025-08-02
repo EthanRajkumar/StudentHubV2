c32176d1cbdc089579a013ae36127286b1751596
EthanRajkumar <rajkumare@wit.edu>
2025-08-02 05:34:25 -0400

    Add generic scene initialization

--- src/main/java/com/example/studenthublogin/SceneSwapper.java
+++ src/main/java/com/example/studenthublogin/SceneSwapper.java
@@ -10,81 +10,9 @@ import java.io.IOException;
 public class SceneSwapper {
     public static Stage stage;
 
-    public static SceneController GetController(String sceneUrl, FXMLLoader loader)
+    public static SceneController GetController(FXMLLoader loader)
     {
-        switch (sceneUrl)
-        {
-            case "hello-view.fxml":
-                return loader.<HelloController>getController();
-
-            case "StudentHubAdminAddCourse.fxml":
-                return loader.<StudentAddCourseController>getController();
-
-            case "StudentHubAdminMenu.fxml":
-                return loader.<StudentMenuController>getController();
-
-            case "StudentHubAdminRemoveCourse.fxml":
-                return loader.<AdminRemoveCourseController>getController();
-
-            case "StudentHubAdminSearchCourse.fxml":
-                return loader.<StudentSearchCourseController>getController();
-
-            case "StudentHubAdminSearchCourseAdv.fxml":
-                return loader.<StudentSearchCourseAdvController>getController();
-
-            case "StudentHubAdminLinkInstructor.fxml":
-                return loader.<AdminLinkInstructorController>getController();
-
-            case "StudentHubAdminUnlinkInstructor.fxml":
-                return loader.<AdminUnlinkInstructorController>getController();
-
-            case "StudentHubAdminRegisterStudent.fxml":
-                return loader.<AdminRegisterStudentController>getController();
-
-            case "StudentHubAdminUnregisterStudent.fxml":
-                return loader.<AdminUnregisterStudentController>getController();
-
-            case "StudentHubInstructorMenu.fxml":
-                return loader.<InstructorMenuController>getController();
-
-            case "StudentHubInstructorSearchCourse.fxml":
-                return loader.<InstructorSearchCourseController>getController();
-
-            case "StudentHubInstructorSearchCourseAdv.fxml":
-                return loader.<InstructorSearchCourseAdvController>getController();
-
-            case "StudentHubInstructorSearchCourseRoster.fxml":
-                return loader.<InstructorShowCourseRosterController>getController();
-
-            case "StudentHubInstructorShowCourseRoster.fxml":
-                return loader.<InstructorShowCourseRosterController>getController();
-
-            case "StudentHubInstructorShowSchedule.fxml":
-                return loader.<InstructorShowScheduleController>getController();
-
-            case "StudentHubLogin.fxml":
-                return loader.<HelloController>getController();
-
-            case "StudentHubStudentAddCourse.fxml":
-                return loader.<StudentAddCourseController>getController();
-
-            case "StudentHubStudentMenu.fxml":
-                return loader.<StudentMenuController>getController();
-
-            case "StudentHubStudentRemoveCourse.fxml":
-                return loader.<StudentRemoveCourseController>getController();
-
-            case "StudentHubStudentSearchCourse.fxml":
-                return loader.<StudentSearchCourseController>getController();
-
-            case "StudentHubStudentSearchCourseAdv.fxml":
-                return loader.<StudentSearchCourseAdvController>getController();
-
-            case "StudentHubStudentShowSchedule.fxml":
-                return loader.<StudentShowScheduleController>getController();
-        }
-
-        return null;
+        return loader.getController();
     }
 
     public static void SwapScene(String sceneUrl)
@@ -94,7 +22,7 @@ public class SceneSwapper {
             Parent root = fxmlLoader.load();
             Scene scene = new Scene(root, 640, 400);
             stage.setScene(scene);
-            SceneController controller = GetController(sceneUrl, fxmlLoader);
+            SceneController controller = GetController(fxmlLoader);
             controller.Initialize(stage);
         }
         catch (IOException e)
